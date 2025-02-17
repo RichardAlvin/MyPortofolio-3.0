8554fa1a1eb41d925dfd01808494fc189e5e54de
richardalvin <richardalvinpratama8@gmail.com>
2025-02-17 14:14:08 +0700

    fixing navbar

--- app/api/articles/[slug]/route.ts
+++ app/api/articles/[slug]/route.ts
@@ -1,11 +1,11 @@
-import data from "@/data/article.json"
-import { NextRequest, NextResponse } from "next/server";
+// import data from "@/data/article.json"
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, context: any) {
-    const { params } = context;
-    const article = data.filter((x) => params.slug === x.slug.toString());
+// export async function GET(req: NextRequest, context: any) {
+//     const { params } = context;
+//     const article = data.filter((x) => params.slug === x.slug.toString());
 
-    return NextResponse.json({
-        article,
-    })
-}
+//     return NextResponse.json({
+//         article,
+//     })
+// }
