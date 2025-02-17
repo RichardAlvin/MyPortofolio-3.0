25169a3c4de1da5fc797e811daf77e62d408bc63
richardalvin <richardalvinpratama8@gmail.com>
2025-02-17 14:19:02 +0700

    fixing navbar

---
+++ app/(root)/works/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+//import Card from '../../../components/Card';
+import DetailPage from '../../../components/DetailPage';
+
+const page = () => {
+  return (
+    <>
+        <DetailPage />
+        <section className="related-articles">
+          <div className="circle-3"></div>
+          <div className="circle-4"></div>
+          <h2>Related Works</h2>
+          <div className="card-container">
+            {/* <Card />
+            <Card />
+            <Card /> */}
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default page
