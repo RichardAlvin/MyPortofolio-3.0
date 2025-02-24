8df81993f19a5b5efb4c3d61c8c10036655e0513
richardalvin <richardalvinpratama8@gmail.com>
2025-02-24 16:43:05 +0700

    fixing api detail article and works

--- app/api/articles/[slug]/route.ts
+++ app/api/articles/[slug]/route.ts
@@ -2,20 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import data from "@/data/article.json";
 
 export async function GET(
-  request: NextRequest,
-  context: { params: { slug?: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const params = await context.params;
 
-  if (!params?.slug) {
+  const slug = (await params).slug;
+  if (!slug) {
     return NextResponse.json({ error: "Slug parameter is missing" }, { status: 400 });
   }
 
-  const article = data.find((x) => x.slug === params.slug);
+  const article = data.find((x) => x.slug === slug);
 
   if (!article) {
     return NextResponse.json({ error: "Article not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ article });
+  return NextResponse.json({ article }, { status: 200 });
 }
--- app/api/works/[slug]/route.ts
+++ app/api/works/[slug]/route.ts
@@ -3,19 +3,19 @@ import data from "@/data/work.json";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { slug?: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const params = await context.params; // Tunggu params agar bisa diakses
 
-  if (!params?.slug) {
+  const slug = (await params).slug;
+  if (!slug) {
     return NextResponse.json({ error: "Slug parameter is missing" }, { status: 400 });
   }
 
-  const work = data.find((x) => x.slug === params.slug);
+  const work = data.find((x) => x.slug === slug);
 
   if (!work) {
     return NextResponse.json({ error: "Work not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ work });
+  return NextResponse.json({ work }, { status: 200 });
 }
